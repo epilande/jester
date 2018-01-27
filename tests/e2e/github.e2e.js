9de6f92249d6f8cf0a919c89cbe4c604c88d888e
Emmanuel Pilande <epilande@gmail.com>
2018-01-26 23:08:39 -0800

    :runner::dash: Add github smoke test

---
+++ tests/e2e/github.e2e.js
@@ -0,0 +1,44 @@
+const puppeteer = require('puppeteer');
+let browser;
+let page;
+
+const github = {
+  url: 'https://github.com/',
+  user: 'epilande',
+  repo: 'yarems',
+};
+
+beforeAll(async () => {
+  browser = await puppeteer.launch();
+  page = await browser.newPage();
+  await page.setViewport({ width: 1280, height: 800 });
+});
+
+afterAll(async () => {
+  await browser.close();
+});
+
+describe('Github view repo', async () => {
+  test('has search input', async () => {
+    await page.goto(github.url);
+    const searchInput = await page.$('.header-search-input');
+    expect(searchInput).toBeTruthy();
+  });
+
+  test('shows search results after search input', async () => {
+    await page.type('.header-search-input', github.repo);
+    const searchInput = await page.$('.header-search-input');
+    await searchInput.press('Enter');
+    await page.waitForNavigation();
+    const repoList = await page.$('.repo-list');
+    expect(repoList).toBeTruthy();
+  });
+
+  test('go to repo', async () => {
+    const repoLink = await page.$(`a[href="/${github.user}/${github.repo}"]`);
+    await repoLink.click();
+    await page.waitForNavigation();
+    const repoContent = await page.$('.repository-content');
+    expect(repoContent).toBeTruthy();
+  });
+});
