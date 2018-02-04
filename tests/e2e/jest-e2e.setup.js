42694d9b461f76a24baef0c8ace8fb2d2bcd089b
Emmanuel Pilande <epilande@gmail.com>
2018-02-04 02:19:01 -0800

    :camera: Set up jest image snapshot

--- tests/e2e/jest-e2e.setup.js
+++ tests/e2e/jest-e2e.setup.js
@@ -1,5 +1,8 @@
+const { toMatchImageSnapshot } = require('jest-image-snapshot');
 const debug = process.env.TEST_ENV === 'debug';
 
 if (debug) {
   jest.setTimeout(15000);
 }
+
+expect.extend({ toMatchImageSnapshot });
